23517e267ca17d7c7911288381f499099e52f53c
Tiago Lascasas dos Santos <tiagolascasas@outlook.com>
2024-12-17 14:14:33 -0500

    Adds missing Linux header files to llama2

--- test/TestLoading.ts
+++ test/TestLoading.ts
@@ -4,27 +4,27 @@ import { SuiteSelector } from "../src/SuiteSelector.js";
 import Query from "@specs-feup/lara/api/weaver/Query.js";
 
 let suite = SuiteSelector.APPS;
-let app = "edgedetect";
+let app = "llama2";
 LiteBenchmarkLoader.load(suite, app);
 
 for (const fun of Query.search(FunctionJp)) {
     console.log(fun.name);
 }
 
-suite = SuiteSelector.ROSETTA;
-app = "spam-filter";
-LiteBenchmarkLoader.load(suite, app);
+// suite = SuiteSelector.ROSETTA;
+// app = "spam-filter";
+// LiteBenchmarkLoader.load(suite, app);
 
-for (const fun of Query.search(FunctionJp)) {
-    console.log(fun.name);
-}
+// for (const fun of Query.search(FunctionJp)) {
+//     console.log(fun.name);
+// }
 
-suite = SuiteSelector.ROSETTA;
-for (const app of suite.apps) {
-    LiteBenchmarkLoader.load(suite, app);
+// suite = SuiteSelector.ROSETTA;
+// for (const app of suite.apps) {
+//     LiteBenchmarkLoader.load(suite, app);
 
-    console.log(`Functions of app ${app} from benchmark suite ${suite.name}:`);
-    for (const fun of Query.search(FunctionJp)) {
-        console.log(fun.name);
-    }
-}
+//     console.log(`Functions of app ${app} from benchmark suite ${suite.name}:`);
+//     for (const fun of Query.search(FunctionJp)) {
+//         console.log(fun.name);
+//     }
+// }
