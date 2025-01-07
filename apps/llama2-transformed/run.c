2e28dde86760bb417f0d275cfbe7bc89594f0d84
Tiago Lascasas dos Santos <tiagolascasas@outlook.com>
2025-01-07 23:01:44 +0000

    Forward and sample done

--- apps/llama2-transformed/run.c
+++ apps/llama2-transformed/run.c
@@ -983,6 +983,72 @@ int sample_topp(float *probabilities, int n, float topp, ProbIndex *probindex, f
     return probindex[last_idx].index; // in case of rounding errors
 }
 
+int sample_topp_no_struct(float *probabilities, int n, float topp, float *sampler_probindex_prob, int *sampler_probindex_index, float coin)
+{
+    // top-p sampling (or "nucleus sampling") samples from the smallest set of
+    // tokens that exceed probability topp. This way we never sample tokens that
+    // have very low probabilities and are less likely to go "off the rails".
+    // coin is a random number in [0, 1), usually from random_f32()
+
+    int n0 = 0;
+    // quicksort indices in descending order of probabilities
+    // values smaller than (1 - topp) / (n - 1) cannot be part of the result
+    // so for efficiency we crop these out as candidates before sorting
+    const float cutoff = (1.0f - topp) / (n - 1);
+    for (int i = 0; i < n; i++)
+    {
+        if (probabilities[i] >= cutoff)
+        {
+            sampler_probindex_index[n0] = i;
+            sampler_probindex_prob[n0] = probabilities[i];
+            n0++;
+        }
+    }
+    // qsort(sampler_probindex_prob, n0, sizeof(ProbIndex), compare);
+    for (size_t i = 0; i < n0 - 1; ++i)
+    {
+        for (size_t j = 0; j < n0 - i - 1; ++j)
+        {
+            if (sampler_probindex_prob[j] > sampler_probindex_prob[j + 1])
+            {
+                float temp = sampler_probindex_prob[j];
+                sampler_probindex_prob[j] = sampler_probindex_prob[j + 1];
+                sampler_probindex_prob[j + 1] = temp;
+
+                int temp1 = sampler_probindex_index[j];
+                sampler_probindex_index[j] = sampler_probindex_index[j + 1];
+                sampler_probindex_index[j + 1] = temp1;
+            }
+        }
+    }
+
+    // truncate the list where cumulative probability exceeds topp
+    float cumulative_prob = 0.0f;
+    int last_idx = n0 - 1; // in case of rounding errors consider all elements
+    for (int i = 0; i < n0; i++)
+    {
+        cumulative_prob += sampler_probindex_prob[i];
+        if (cumulative_prob > topp)
+        {
+            last_idx = i;
+            break; // we've exceeded topp by including last_idx
+        }
+    }
+
+    // sample from the truncated list
+    float r = coin * cumulative_prob;
+    float cdf = 0.0f;
+    for (int i = 0; i <= last_idx; i++)
+    {
+        cdf += sampler_probindex_prob[i];
+        if (r < cdf)
+        {
+            return sampler_probindex_index[i];
+        }
+    }
+    return sampler_probindex_index[last_idx]; // in case of rounding errors
+}
+
 void build_sampler(Sampler *sampler, int vocab_size, float temperature, float topp, unsigned long long rng_seed)
 {
     sampler->vocab_size = vocab_size;
@@ -1041,6 +1107,47 @@ int sample(Sampler *sampler, float *logits)
         {
             // top-p (nucleus) sampling, clamping the least likely tokens to zero
             next = sample_topp(logits, sampler->vocab_size, sampler->topp, sampler->probindex, coin);
+        }
+    }
+    return next;
+}
+
+int sample_no_struct(int sampler_vocab_size,
+                     float sampler_temperature,
+                     float sampler_topp,
+                     unsigned long long sampler_rng_state,
+                     float *sampler_probindex_prob,
+                     int *sampler_probindex_index,
+                     float *logits)
+{
+    // sample the token given the logits and some hyperparameters
+    int next;
+    if (sampler_temperature == 0.0f)
+    {
+        // greedy argmax sampling: take the token with the highest probability
+        next = sample_argmax(logits, sampler_vocab_size);
+    }
+    else
+    {
+        // apply the temperature to the logits
+        for (int q = 0; q < sampler_vocab_size; q++)
+        {
+            logits[q] /= sampler_temperature;
+        }
+        // apply softmax to the logits to get the probabilities for next token
+        softmax(logits, sampler_vocab_size);
+        // flip a (float) coin (this is our source of entropy for sampling)
+        float coin = random_f32(&sampler_rng_state);
+        // we sample from this distribution to get the next token
+        if (sampler_topp <= 0 || sampler_topp >= 1)
+        {
+            // simply sample from the predicted probability distribution
+            next = sample_mult(logits, sampler_vocab_size, coin);
+        }
+        else
+        {
+            // top-p (nucleus) sampling, clamping the least likely tokens to zero
+            next = sample_topp_no_struct(logits, sampler_vocab_size, sampler_topp, sampler_probindex_prob, sampler_probindex_index, coin);
         }
     }
     return next;
@@ -1109,8 +1216,8 @@ int cluster(
     float sampler_temperature,
     float sampler_topp,
     unsigned long long sampler_rng_state,
-    float sampler_probindex_prob,
-    int sampler_probindex_index,
+    float *sampler_probindex_prob,
+    int *sampler_probindex_index,
     // other args
     int *prompt_tokens,
     int num_prompt_tokens,
@@ -1166,7 +1273,13 @@ int cluster(
         else
         {
             // otherwise sample the next token from the logits
-            next = sample(sampler, logits);
+            next = sample_no_struct(sampler_vocab_size,
+                                    sampler_temperature,
+                                    sampler_topp,
+                                    sampler_rng_state,
+                                    sampler_probindex_prob,
+                                    sampler_probindex_index,
+                                    logits);
         }
         pos++;
 
@@ -1269,8 +1382,13 @@ void generate(Transformer *transformer, Tokenizer *tokenizer, Sampler *sampler, 
     unsigned long long sampler_rng_state = sampler->rng_state;
 
     // *sampler_probindex
-    float sampler_probindex_prob = sampler_probindex->prob;
-    int sampler_probindex_index = sampler_probindex->index;
+    float *sampler_probindex_prob = (float *)malloc(sampler_vocab_size * sizeof(float));
+    int *sampler_probindex_index = (int *)malloc(sampler_vocab_size * sizeof(int));
+    for (int i = 0; i < sampler_vocab_size; i++)
+    {
+        sampler_probindex_prob[i] = sampler_probindex[i].prob;
+        sampler_probindex_index[i] = sampler_probindex[i].index;
+    }
 
     long start = time_in_ms();
     // --------------------------------------------------------------------------------------------
