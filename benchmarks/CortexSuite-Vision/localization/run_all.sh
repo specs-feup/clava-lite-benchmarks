1e3489d4def1e47e8c4c7ccb48e550835c866b4f
tiagolascasas <tiagolascasas@outlook.com>
2025-07-02 04:54:53 +0100

    CortexSuite makefiles creating input-dependent executables

--- benchmarks/CortexSuite-Vision/localization/run_all.sh
+++ benchmarks/CortexSuite-Vision/localization/run_all.sh
@@ -3,10 +3,15 @@
 DATA_DIR="data"
 
 for dir in "$DATA_DIR"/*/; do
-    # Remove trailing slash and path prefix
     input_name=$(basename "$dir")
+
+    if [ "$input_name" = "GroundData" ]; then
+        echo "Skipping $input_name"
+        continue
+    fi
 
     echo "=== Running: make run INPUT=$input_name ==="
     make run INPUT="$input_name"
+    make clean INPUT="$input_name"
     echo
 done
